3344cb779b97158479ccc2bb33acfd7a085a915c
Lucas Baptista <deadpool.medicine@gmail.com>
2020-05-30 20:41:18 +0100

    Grafico provisoriamente no dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+use App\Http\Movimento;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $chart_options = [
+            'chart_title' => 'Transactions by dates',
+            'report_type' => 'group_by_date',
+            'model' => 'App\Http\Movimento',
+            'group_by_field' => 'data',
+            'group_by_period' => 'day',
+            'aggregate_function' => 'sum',
+            'aggregate_field' => 'valor',
+            'chart_type' => 'line',
+        ];
+
+    $chart = new LaravelChart($chart_options);
+        
+        return view('dashboard.index', compact('chart'));
     }
 }
