1e27b0f3a91a28433219b70364521ef431aeeca0
MachadoAndre92 <45774991+MachadoAndre92@users.noreply.github.com>
2020-06-01 15:37:22 +0100

    Fixed

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -16,21 +16,10 @@ class UserPolicy
         return $user->adm;
     }
 
-<<<<<<< HEAD
-    //impedir que um admin possa alterar os seus proprios tipos (adm,bloqueado) -- not working
-    public function viewOthers(User $user,User $user2)
-    {
-        //dd($listId);
-        $id1 = $user->id;
-        $id2 = $user2->id;
-       return ($user->adm == "Admin") && ($id1 != $id2);
-
-=======
     //impedir que um admin possa alterar os seus proprios tipos (adm,bloqueado) ... se eu estiver a pensar bem --not working
     public function view(User $user, $id)
     {
         return true;
->>>>>>> parent of f89cadc... Merge branch 'master' of https://github.com/LuisBernarda/ProjetoAI_Net
     }
 
     //assegura que so o proprio utilizador possa alterar os seus dados, mais segurança
@@ -42,6 +31,6 @@ class UserPolicy
     //assegura que so o proprio utilizador possa apagar os seus dados, mais segurança
     public function delete(User $user1, $id)
     {
-        return ($user1->id == $id);    
+        return ($user1->id == $id);
     }
 }
